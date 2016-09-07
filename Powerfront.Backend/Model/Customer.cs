f40c4b6d25b64157c74b63d8b3ada4b2013ed812
Austin Dimmer <austin@effective-computing.com>
2016-09-07 18:56:29 +0200

    Added some uget Packages and Fleshed out Solution Structure

---
+++ Powerfront.Backend/Model/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Powerfront.Backend.Model
+{
+    public class Customer : ICustomer
+    {
+        public string Age { get; set; }
+
+        public string Name { get; set; }
+    }
+}
